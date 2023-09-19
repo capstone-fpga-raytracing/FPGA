e75d9c5a586dddc82eee94056e94c4e96d76d33c
MtahaK <taha.khokhar@mail.utoronto.ca>
2023-09-19 19:59:50 -0400

    Wrote 32 bit fixed point adder with overflow detection

--- fip_opts.sv
+++ fip_opts.sv
@@ -1,1 +1,21 @@
-test
+module fip_32_adder(
+    input signed [31:0] x,
+    input signed [31:0] y,
+    output reg signed [31:0] sum,
+    output reg overflow
+);
+    parameter integer_bits = 16;
+    parameter fractional_bits = 16;
+    
+    always @(*)
+    begin
+        sum = x + y
+
+        if (result > max_value || result < min_value)
+            overflow = 1'b1;
+        else
+            overflow = 1'b0;
+    end
+    
+endmodule
+
