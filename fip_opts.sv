9605985f61a01a33daa7aa615e2bd165a012f1c8
Taha Khokhar <taha.khokhar@mail.utoronto.ca>
2023-09-20 15:16:27 -0400

    Added overflow detection to multiplication module

--- fip_opts.sv
+++ fip_opts.sv
@@ -24,13 +24,23 @@ endmodule
 module fip_32_mult(
     input signed [31:0] x, 
     input signed [31:0] y,
-    output signed [31:0] prod
+    output signed [31:0] prod,
+    output overflow
 );
-    logic signed [63:0] temp;
+    logic signed [63:0] temp, adjusted;
 
     always_comb begin
         temp = x * y;
-        prod = temp >> 16;
+        adjusted = temp >>> 16;
+
+        // check for overflow
+        if (adjusted[31] == 1'b0) begin // Number should be positive, check if any sign bit is 1
+            overflow = |adjusted[63:32]  // OR all bits. If 1, overflow detected.
+        end else begin // Number should be negative, check if any sign bit is 0
+            overflow ~!adjusted[63:32] // NOR: If any bit is 0, when it should be 1, overflow detected
+        end
+
+        prod = adjusted[31:0]
     end
 
 
